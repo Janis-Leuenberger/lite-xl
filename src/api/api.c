0f2d5b93c9ac359c3e8e6c1e09b986947e0e6614
jgmdev <jgmdev@gmail.com>
2021-05-19 05:44:52 -0400

    Initial work to allow writing LSP client in LUA.
    * Added an amalgamated C version of https://github.com/DaanDeMeyer/reproc
    * Added lua binding of reproc accesible in the process package.
    * Modified meson build to include reproc sources as src/api/process.c

--- src/api/api.c
+++ src/api/api.c
@@ -3,11 +3,13 @@
 
 int luaopen_system(lua_State *L);
 int luaopen_renderer(lua_State *L);
+int luaopen_process(lua_State *L);
 
 
 static const luaL_Reg libs[] = {
   { "system",    luaopen_system     },
   { "renderer",  luaopen_renderer   },
+  { "process",   luaopen_process    },
   { NULL, NULL }
 };
 
